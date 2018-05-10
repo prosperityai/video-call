3c0af0108c910cc54920f27c7ed179338c13d438
prosperityai <prosperpaul@ymail.com>
2018-05-10 08:02:01 +0300

    server

---
+++ index.js
@@ -0,0 +1,41 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const Pusher = require('pusher');
+const app = express();
+
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// Session middleware
+
+// Create an instance of Pusher
+const pusher = new Pusher({
+    appId: '523383',
+    key: 'd5dc12b8659f90dc4e75',
+    secret: '2e12a5017c5eec1275ab',
+    cluster: 'ap2',
+    encrypted: true
+});
+
+
+app.get('/', (req, res) => {
+    return res.sendFile(__dirname + '/index.html');
+});
+
+// get authentictation for the channel;
+app.post('/pusher/auth', (req, res) => {
+        const socketId = req.body.socket_id;
+        const channel = req.body.channel_name;
+        var presenceData = {
+            user_id: Math.random().toString(36).slice(2) + Date.now()
+        }
+        const auth = pusher.authenticate(socketId, channel, presenceData);
+        res.send(auth);
+});
+
+//listen on the app
+app.listen(3000, () => {
+    return console.log('Server is up on 3000')
+});
